e7d790e0439369d076d87a3e7b59e53acba3dec0
SuYoungHong <shong@mozilla.com>
2019-12-03 19:04:12 -0800

    sadlkjf

---
+++ fx_usage_report_testing.sh
@@ -0,0 +1,8 @@
+pip install py4j --upgrade
+pip install numpy==1.16.4
+pip install python-dateutil==2.5.0
+pip install pytz==2011k
+pip install --no-dependencies pandas==0.24
+
+git clone https://www.github.com/mozilla/Fx_Usage_Report.git
+cd Fx_Usage_Report
